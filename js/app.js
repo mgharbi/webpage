edf30030445f377819bffa03b736a85b63468932
Michael Gharbi <gharbi@mit.edu>
2015-08-26 18:07:38 -0400

    partials

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-angular.module('mgharbi', []);
+var mgharbi_app = angular.module('mgharbi', [
+    'ngRoute',
+    'mgharbi.controllers'
+]);
+
+mgharbi_app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('', {
+                templateUrl: 'partials/home.html',
+                controller: 'MainController'
+            }).
+            when('/projects', {
+                templateUrl: 'partials/home.html',
+                controller: 'MainController'
+            }).
+            when('/bio', {
+                templateUrl: 'partials/bio.html',
+                controller: 'MainController'
+            }).
+            otherwise({
+                redirectTo: ''
+            });
+}]);
--- js/main.ctrl.js
+++ js/main.ctrl.js
@@ -1,8 +1,17 @@
-angular.module('mgharbi').controller("MainController", function() {
+var mgharbi_ctrl = angular.module('mgharbi.controllers', []);
+
+mgharbi_ctrl.controller("MainController", function() {
     var vm       = this;
     vm.firstname = "Michaël";
     vm.lastname  = "Gharbi";
     vm.title     = "PhD candidate - MIT CSAIL";
     vm.email     = "gharbi@mit.edu";
     vm.phone = "+1 (857)-210-9559";
+});
+
+mgharbi_ctrl.controller("NavController", function($scope, $location) {
+    var vm       = this;
+    vm.isActive = function (viewLocation) { 
+            return viewLocation === $location.path();
+    };
 });
