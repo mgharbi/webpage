1df132e9d88b66b1cb86397103c1b2de69246867
Michael Gharbi <gharbi@mit.edu>
2015-11-11 23:29:04 -0500

    update link

--- js/main.ctrl.js
+++ js/main.ctrl.js
@@ -11,6 +11,7 @@ mgharbi_ctrl.controller("MainController",   ['$scope', 'Projects', function($sco
     vm.facebook  = "https://www.facebook.com/michael.gharbi.5";
     vm.twitter   = "https://twitter.com/m_gharbi";
     vm.linkedin  = "https://www.linkedin.com/in/michaelgharbi";
+    vm.map       = "https://www.google.com/maps/place/32+Vassar+St,+Cambridge,+MA+02139/@42.3618641,-71.0927513,17z/data=!3m1!4b1!4m2!3m1!1s0x89e370abe1db5703:0xd3b042dc26a8b515?hl=en";
 
 
     // Get project list
